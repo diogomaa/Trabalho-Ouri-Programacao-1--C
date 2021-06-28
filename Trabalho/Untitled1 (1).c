7ceef286b3e3d21c58d49a861a5a6e66dfcbcf0b
diogomaa <46852735+diogomaa@users.noreply.github.com>
2021-06-28 12:52:30 +0100

    Ouri atualização

---
+++ Trabalho/Untitled1 (1).c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+#include<locale.h>
+ 
+main(){
+	system("CLS");
+	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");printf("\xB2\xB2\n");
+	printf("\xB2\xB2\n");
+	printf("\xB2\xB2 UUUUUUUUUUUUU  UU         UU  UUUUUUUUUUUUU  UUUUUUUUUU          \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU             \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU            \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU             \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU              \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UUUUUUUUUUUUU      UU                 \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU  UU             UU        UUUUUUUUU  UUUUUUU UU     UU UUUUUU    \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU   UU            UU        UU         UU   UU UUUU  UUU UU      \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU    UU           UU        UU         UU   UU UU  UU UU UUUUUU \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU     UU          UU        UU  UUUUU  UUUUUUU UU     UU UU    \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU      UU         UU        UU     UU  UU   UU UU     UU UU    U\n");
+	printf("\xB2\xB2 UUUUUUUUUUUUU  UUUUUUUUUUUUU  UU       UU    UUUUUUUUUU    UUUUUUUUU  UU   UU UU     UU UUUUUU\n");
+	printf("\xB2\xB2               \n");
+	printf("\xB2\xB2 UU                  \n");
+	printf("\xB2\xB2 UU                       \n");
+	printf("\xB2\xB2 UU                          \n");
+	printf("\xB2\xB2 UU                           \n");
+	printf("\xB2\xB2               \n");
+	printf("\xB2\xB2            \n");
+	printf("\xB2\xB2             \n");
+	printf("\xB2\xB2                 \n");
+	printf("\xB2\xB2                \n");
+	printf("\xB2\xB2              \n");
+	printf("\xB2\xB2             \n");
+	printf("\xB2\xB2             \n");
+	printf("\xB2\xB2             \n");
+	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
+	
+	
+}
---
+++ Trabalho/Untitled1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+#include<locale.h>
+ 
+main(){
+	system("CLS");
+	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
+	printf("\xB2\xB2\n");
+	printf("\xB2\xB2         \n");
+	printf("\xB2\xB2 UUUUUUUUUUUUU  UU         UU  UUUUUUUUUUUUU  UUUUUUUUUU                                             \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU                                                 \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU                                                              \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU                                                               \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU         UU      UU                                                           \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UUUUUUUUUUUUU      UU                                                         \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU  UU             UU        UUUUUUUUU  UUUUUUU UU    UU UUUUUU                    \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU   UU            UU        UU         UU   UU UUU  UUU UU                   \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU    UU           UU        UU         UU   UU UU UU UU UUUUU             \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU     UU          UU        UU  UUUUU  UUUUUUU UU    UU UU                        \n");
+	printf("\xB2\xB2 UU         UU  UU         UU  UU      UU         UU        UU     UU  UU   UU UU    UU UU             \n");
+	printf("\xB2\xB2 UUUUUUUUUUUUU  UUUUUUUUUUUUU  UU       UU    UUUUUUUUUU    UUUUUUUUU  UU   UU UU    UU UUUUUU             \n");
+	printf("\xB2\xB2               \n");
+	printf("\xB2\xB2 UU              UUUUUU      UUUUUUU                     UUUUUUU      UUUUUU                                 \n");
+	printf("\xB2\xB2 UU              UU   UU     UU   UU                     UU   UU      UU                         \n");
+	printf("\xB2\xB2 UU              UU   UU     UU   UU                     UU   UU      UU                                  \n");
+	printf("\xB2\xB2 UUUUUU UU   UU  UU   UU     UUUUUUU                     UUUUUUU      UU      YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY\n");
+	printf("\xB2\xB2 UU  UU  UU UU   UU   UU     UU   UU                     UU   UU      UU      YY UU     UU UUUUUUU UU    UU UU  UU\n");
+	printf("\xB2\xB2 UUUUUU   UUU    UUUUUU  UU  UU   UU                     UU   UU  UU  UUUUUU  YY UUUU  UUU UU      UUUU  UU UU  UU UU\n");
+	printf("\xB2\xB2         UU                         UUUUUU UU   UU UUUUU                      YY UU  UU UU UUUUU   UU UU UU UU  UU \n");
+	printf("\xB2\xB2        UU                          UU  UU UUUU UU UU  UU                     YY UU     UU UU      UU  UUUU UU  UU UU\n");
+	printf("\xB2\xB2                                    UUUUUU UU UUUU UU  UU                     YY UU     UU UUUUUUU UU    UU UUUUUU \n");
+	printf("\xB2\xB2                                    UU  UU UU  UUU UU  UU                     YY                                    \n");
+	printf("\xB2\xB2                                    UU  UU UU   UU UUUUU                      YY                                    \n");
+	printf("\xB2\xB2                                                                              YY                                     \n");
+	printf("\xB2\xB2                                                                              YY                                     \n");
+	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
+	
+	
+}
